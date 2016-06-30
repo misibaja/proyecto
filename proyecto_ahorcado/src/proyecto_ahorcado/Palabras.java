bf4b687753d7adf598491c27732269791d05ee28
misibaja <msibaja@ucatolica.ac.cr>
2016-06-29 22:05:35 -0600

    prueba de github
    tengo un error :-1:

--- proyecto_ahorcado/src/proyecto_ahorcado/Palabras.java
+++ proyecto_ahorcado/src/proyecto_ahorcado/Palabras.java
@@ -7,37 +7,87 @@ package proyecto_ahorcado;
 //profe voy a comentariar asi de momento es como para guiarme y tener una idea clara luego lo chaneo 
 
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Minor Sibaja Clavo
  */ //22/06/2016
 public class Palabras {
-
-    private string palabras = "", Solucion = "";
+    
+    private String palabras = "", Solucion = "";
     private int posicion = -1, intentos = 0, maximoDeIntentos = 2;
-    private string[] PalabrasAdivinar = {"GEARS OF WAR", "HALO", "FIFA", "AEROPLANO", "VAGON", "SUPRA", "VIVA SAPRISSA", "EEUU", "CEREZA", "FLATULENCIA", "INODORO",
-        "BESTIA", "DOG", "CATES", "IRON MAIDEM", "ACDC"};  //este arreglo es para las palabras, se supone que los guardo en una variable
+    private String[] PalabrasAdivinar = {"GEARS OF WAR", "HALO", "FIFA", "AEROPLANO", "VAGON", "SUPRA", "VIVA SAPRISSA"};  //este arreglo es para las palabras, se supone que los guardo en una variable
+    public int Gano = -1;
 
     public void ComenzarJuego() {     //metodo para que me devuelva la funcion random
         Random R = new Random();
         int i = -1;                     // declaro una variable i(intentos como deducion) para realizar la la busqueda de dicha palabra
-        do {                            //inicio un ciclo tipo (do) para que intentos sea igual a R.nextInt(palabras) en teoria el bucle
+        do{                             //inicio un ciclo tipo (do) para que intentos sea igual a R.nextInt(palabras) en teoria el bucle
             //se ejecuta obligatoriamente
-            i = R.nextInt(PalabrasAdivinar);
-        } while (posicion == i);            //si la condicion posicion se cumple, posicion va ser igual a i(intentos)
+            i = R.nextInt(PalabrasAdivinar.length);
+            
+        }while (posicion == i);            //si la condicion posicion se cumple, posicion va ser igual a i(intentos)
         posicion = i;
-        palabras = PalabrasAdivinadar[posicion]; // y palbras va ser igual a PalabrasAdivinadar
-        for (i = 0; i < palabras.length(); i++); //el ciclo for que es i(intentos) va ser igual a cero, donde inicia posicion, la condicion i va ser menor a palabras (relaciona),
-        {                                           // y la ultima va manipular los valores que mencionamos
-            if (palabras.charAt(i) != ' ') 
-                                                    // si se cumple palabras va ser igual a '' y solucion = caract sino = espacio para generar las palabras
+        palabras = PalabrasAdivinar[posicion];
+        
+            // y palbras va ser igual a PalabrasAdivinadar
+        for (i = 0; i < palabras.length(); i++);{ 
+                                                    //el ciclo for que es i(intentos) va ser igual a cero, donde inicia posicion, la condicion i va ser menor a palabras (relaciona),{                                           // y la ultima va manipular los valores que mencionamos
+             if(palabras.charAt(i)!)                 // si se cumple palabras va ser igual a '' y solucion = caract sino = espacio para generar las palabras
                 Solucion += ("_");                     // debemos pensar que es muy necesario por el hecho de los espacios si por ejemplo gears of war 
-            else
-                Solucion+=(" ");
+                else 
+                Solucion += (" ");
+    
+            }
+    
+            Dibujar();
+          
+    }
+            
+    public void BuscarLetras(char pal) {
+        String sol1 = Solucion;
+        Solucion = "";
+        for (int x = 0; x < palabras.length(); x++) {
+
+            if (palabras.charAt(x) != ' ') {
+
+                if (pal == palabras.charAt(x)) 
+                    Solucion += pal;
+               else 
+                    Solucion += sol1.charAt(x);
+                
+
+            }else 
+                Solucion += ' ';
+            
 
         }
+        if (sol1.compareTo(Solucion) == 0 && Solucion.indexOf(pal) == -1) 
+            intentos++;
+        Dibujar();
+        if (Solucion.compareTo(palabras) == 0) {
+            Gano = 0;
+            JOptionPane.showMessageDialog(null, "USTED A GANADO!!");
+        } else if (intentos == maximoDeIntentos) {
+            Gano = 1;
+            JOptionPane.showMessageDialog(null, "USTED A PERDIDO!!");
+        }
 
+        
+    }
+
+    public void Dibujar() {
+        for (char c : Solucion.toCharArray()) 
+            System.out.print(c + " ");
+        
+        SOP(" ");
+        SOP("\nIntentos restantes: " + (maximoDeIntentos - intentos));
+
+    }
+
+    public void SOP(String msj) {
+        System.out.println(msj);
     }
 
 }
--- proyecto_ahorcado/src/proyecto_ahorcado/Proyecto_ahorcado.java
+++ proyecto_ahorcado/src/proyecto_ahorcado/Proyecto_ahorcado.java
@@ -3,11 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package proyecto_ahorcado;
 
-import java.util.Random;
-
+import java.util.Scanner;
 
 /**
  *
@@ -20,6 +18,35 @@ public class Proyecto_ahorcado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner Leer = new Scanner(System.in);
+        Palabras A = new Palabras();
+        String letra = "";
+        char Letra;
+
+        int op = -1;
+        do {
+            if (op == -1) {
+                op = 1;
+            } else {
+                A.SOP("\n[1] - Jugar de Nuevo\n[0] - Salir");
+                op = Leer.nextInt();
+            }
+            switch (op) {
+                case 1:
+                    A.ComenzarJuego();
+                    while (A.Gano == -1) {
+                        letra = Leer.next().toUpperCase();
+                        Letra = letra.charAt(0);
+                        A.BuscarLetras(Letra);
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    A.SOP("Opcion Indefinida!");
+            }
+
+        } while (op != 0);
+
     }
-    
 }
